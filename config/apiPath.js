4ba532f3ee3f777931eafe36972deef167f83a13
wangqiuying <wangqiuying@ihmair.cn>
2019-07-08 17:05:45 +0800

    no message

--- config/apiPath.js
+++ config/apiPath.js
@@ -1,35 +1,39 @@
-
 const apiPath = require("./config.js").apiPath;
 
 module.exports = {
-  /**
-   * 获取验证码 get
-   * phone
-   */
-  getCaptcha: apiPath + "/user/getCaptcha",
-  /**
-   * 登录 post
-   * phone
-   * captcha
-   */
-  login: apiPath + "/user/login",
-  /**
-   * 刷新token post
-   */
-  changeToken: apiPath + "/user/changeToken",
+    //获取验证码 get {phone}
+    getCaptcha: apiPath + "/user/getCaptcha",
+
+    //登录 post {phone,captcha}
+    login: apiPath + "/user/login",
+
+    //刷新token post {refreshToken}
+    changeToken: apiPath + "/user/changeToken",
 
 
 
-  /**
-   * 获取用户信息 get
-   * id
-   */
-  getUser: apiPath + "/user/personal/getUser",
+
+    //获取用户信息 get {id}
+    getUser: apiPath + "/user/personal/getUser",
+
+    //编辑用户信息 post {form}
+    editUser: apiPath + "/user/personal/editUser",
+
+    //上传头像 post {id, icon}
+    editIcon: apiPath + "/user/personal/editIcon",
+
+    //获取订单列表(懒加载) get {id,type}
+    getOrderList: apiPath + "/user/personal/getOrderList",
+
+    //获取单个订单信息 get {id}
+    getOrder: apiPath + "/user/personal/getOrder",
 
 
 
-  /**
-   * 获取服务分类 get
-   */
-  getCategory: apiPath + "/user/service/getCategory",
+
+    
+    //获取服务分类 get
+    getCategory: apiPath + "/user/service/getCategory",
+
+
 }
