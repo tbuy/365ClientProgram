de5347b27b6a821105d3fce44808cae957237eef
snowy <wqy445@gmail.com>
2019-08-26 21:47:52 +0800

    no message

--- mpvue365/src/components/banner.vue
+++ mpvue365/src/components/banner.vue
@@ -1,51 +1,61 @@
 <template>
     <div class="swiper">
-        <swiper :indicator-dots="swiperOption.indicatorDots" :autoplay="swiperOption.autoplay" :interval="swiperOption.interval"
-            :duration="swiperOption.duration">
-            <a v-for="item in swiperOption.imgUrls" :key="item.id">
+        <swiper
+            :indicator-dots="swiperOption.indicatorDots"
+            :autoplay="swiperOption.autoplay"
+            :interval="swiperOption.interval"
+            :duration="swiperOption.duration"
+        >
+            <block v-for="item in swiperOption.imgUrls" :key="item.id">
                 <swiper-item>
-                    <image :src="item" class="slide-image" />
+                    <img :src="item" class="slide-image">
                 </swiper-item>
-            </a>
+            </block>
         </swiper>
     </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            swiperOption: {
-                type: Object,
-                default: {
-                    imgUrls: [
-                        'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-                        'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-                    ],
-                    //轮播点
-                    indicatorDots: true,
-                    autoplay: false,
-                    //时间间隔
-                    interval: 5000,
-                    //滑动时长
-                    duration: 400,
-                }
-            }
-        }
+export default {
+  props: {
+    swiperOption: {
+      type: Object,
+      default: {
+        imgUrls: [
+          "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640",
+          "https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640"
+        ],
+        //轮播点
+        indicatorDots: true,
+        autoplay: true,
+        //时间间隔
+        interval: 3000,
+        //滑动时长
+        duration: 400
+      }
     }
-
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    .swiper {
-        width: 100%;
-        height: 320rpx;
-        border-radius: 10rpx;
-        overflow: hidden;
-        & .slide-image {
-            width: 100%;
-            height: 100%;
-            border-radius: 10rpx;
-        }
-    }
-
+.swiper {
+  width: 100%;
+  height: 320rpx;
+  border-radius: 10rpx;
+  overflow: hidden;
+  & swiper {
+    width: 100%;
+    height: 320rpx;
+  }
+  & swiper-item {
+    width: 100%;
+    height: 320rpx;
+  }
+  & .slide-image {
+    width: 100%;
+    height: 320rpx;
+    border-radius: 10rpx;
+  }
+}
 </style>
--- mpvue365/src/pages/index/index.vue
+++ mpvue365/src/pages/index/index.vue
@@ -1,55 +1,20 @@
 <template>
     <div class="container">
-        <div class="swiper">
-            <swiper
-                :indicator-dots="swiperOption.indicatorDots"
-                :autoplay="swiperOption.autoplay"
-                :interval="swiperOption.interval"
-                :duration="swiperOption.duration">
-                <a v-for="item in swiperOption.imgUrls" :key="item.id">
-                    <swiper-item>
-                        <image :src="item" class="slide-image"/>
-                    </swiper-item>
-                </a>
-            </swiper>
-        </div>
+        <v-banner></v-banner>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                swiperOption: {
-                    imgUrls: [
-                        'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-                        'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-                    ],
-                    //轮播点
-                    indicatorDots: true,
-                    autoplay: false,
-                    //时间间隔
-                    interval: 5000,
-                    //滑动时长
-                    duration: 400,
-                }
-            }
-        }
-    }
-
+import banner from "@/components/banner";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    "v-banner": banner
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    .swiper {
-        width: 100%;
-        height: 320rpx;
-        border-radius: 10rpx;
-        overflow: hidden;
-        & .slide-image {
-            width: 100%;
-            height: 100%;
-            border-radius: 10rpx;
-        }
-    }
-
 </style>
